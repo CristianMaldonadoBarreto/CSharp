67d7608b790a6011f0027632f69b6749a4c50866
CristianMaldonadoBarreto <cmb.maldonado@gmail.com>
2023-08-07 20:42:12 -0400

    Add project files.

---
+++ CSharp/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Algorithms;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = new List<int> { 100, 23, 36, 14, 5, 69, 7, 8, 9, 1000, 11, 12, 133, 214, 515, 311 };
+
+            nums = Do_Merge_Sort(nums);
+
+            for (int i = 0; i < nums.Count; i++)
+            {
+                Console.Write(nums[i] + " ");
+            }
+
+        }
+
+        static List<int> Do_Merge_Sort(List<int> arr)
+        {
+            List<int> list = SortingAlgorithms.Merge_Sort(arr);
+
+            return list;
+        }
+
+        static int[]? Do_Bubble_Sort(int[] arr)
+        {
+            return null;
+        }
+
+        static int[]? Do_Quick_Sort(int[] arr)
+        {
+            return null;
+        }
+
+        static int[]? Do_Selection_Sort(int[] arr)
+        {
+            return null;
+        }
+
+        static int[]? Do_Insertion_Sort(int[] arr)
+        {
+            return null;
+        }
+    }
+}
---
+++ SortingAlgorithms/SortingAlgorithms.cs
@@ -0,0 +1,151 @@
+﻿using System.Diagnostics;
+
+namespace Algorithms
+{
+    public static class SortingAlgorithms
+    {
+        static Stopwatch stopwatch = new Stopwatch();
+
+        public static List<T> Merge_Sort<T>(List<T> arr)
+        {
+            Start_Timer();
+
+            #region Method
+
+            //Base Check
+            if (arr.Count <= 1)
+            {
+                return arr;
+            }
+
+            List<T> left = new List<T>();
+            List<T> right = new List<T>();
+
+            for (int i = 0; i < arr.Count; i++)
+            {
+                if (i < arr.Count / 2)
+                {
+                    left.Add(arr[i]);
+                }
+                else
+                {
+                    right.Add(arr[i]);
+                }
+            }
+
+            left = Merge_Sort(left);
+            right = Merge_Sort(right);
+
+            List<T> tempList = Merge(left, right);
+            #endregion
+
+            Stop_Timer();
+            Debug.WriteLine("Merge Sort executed in " + stopwatch.Elapsed.ToString());
+            stopwatch.Restart();
+
+            return tempList;
+        }
+
+        public static List<T> Merge<T>(List<T> _left, List<T> _right)
+        {
+            List<T> result = new List<T>();
+
+            while (_left.Count != 0 && _right.Count != 0)
+            {
+                if (Convert.ToInt32(_left.First()) <= Convert.ToInt32(_right.First()))
+                {
+                    result.Add(_left.First());
+                    _left.RemoveAt(0);
+
+                }
+                else
+                {
+                    result.Add(_right.First());
+                    _right.RemoveAt(0);
+                }
+            }
+
+            while (_left.Count != 0)
+            {
+                result.Add(_left.First());
+                _left.RemoveAt(0);
+            }
+
+            while (_right.Count != 0)
+            {
+                result.Add(_right.First());
+                _right.RemoveAt(0);
+            }
+
+            return result;
+        }
+
+
+
+        public static List<T>? Bubble_Sort<T>(List<T> arr)
+        {
+            Start_Timer();
+
+            #region Method
+
+            #endregion
+
+            Stop_Timer();
+            Debug.WriteLine("Bubble Sort executed in " + stopwatch.Elapsed.TotalSeconds + " SECONDS");
+            stopwatch.Restart();
+            return null;
+        }
+
+        public static List<T>? Insertion_Sort<T>(List<T> arr)
+        {
+            Start_Timer();
+
+            #region Method
+
+            #endregion
+
+            Stop_Timer();
+            Debug.WriteLine("Insertion Sort executed in " + stopwatch.Elapsed.TotalSeconds + " SECONDS");
+            stopwatch.Restart();
+            return null;
+        }
+
+        public static List<T>? Quick_Sort<T>(List<T> arr)
+        {
+            Start_Timer();
+
+            #region Method
+
+            #endregion
+
+            Stop_Timer();
+            Debug.WriteLine("Quick Sort executed in " + stopwatch.Elapsed.TotalSeconds + " SECONDS");
+            stopwatch.Restart();
+            return null;
+        }
+
+        public static List<T>? Selection_Sort<T>(List<T> arr)
+        {
+            Start_Timer();
+
+            #region Method
+
+            #endregion
+
+            Stop_Timer();
+            Debug.WriteLine("Selection Sort executed in " + stopwatch.Elapsed.TotalSeconds + " SECONDS");
+            stopwatch.Restart();
+            return null;
+        }
+
+        public static void Start_Timer()
+        {
+            stopwatch.Start();
+        }
+
+        public static void Stop_Timer()
+        {
+            stopwatch.Stop();
+        }
+    }
+}
